a42f8b205b12bf5b8c53e7b5929dce86e0216498
Colin Jones <trptcolin@gmail.com>
2010-06-07 08:14:06 -0500

    insert and get row

--- src/cassandra/core.clj
+++ src/cassandra/core.clj
@@ -1,24 +1,25 @@
 (ns cassandra.core
   (:import
     (org.apache.cassandra.thrift
-         ConsistencyLevel Cassandra$Client ColumnPath)
+         ConsistencyLevel Cassandra$Client ColumnPath
+         SlicePredicate SliceRange ColumnParent)
     (org.apache.thrift.transport TSocket)
     (org.apache.thrift.protocol TBinaryProtocol)))
 
 (def encoding "UTF-8")
 (def clevel ConsistencyLevel/ONE)
 
-(def *host* "localhost")
-(def *port* 9160)
-(def *keyspace* "Keyspace1")
+(def *client*)
+(def *keyspace*)
 
-(defmacro with-client [[client [host port]] & body]
+(defmacro with-client [[host port keyspace] & body]
   `(let [socket# (TSocket. ~host ~port)
-         protocol# (TBinaryProtocol. socket#)
-         ~'client (Cassandra$Client. protocol#)]
-     (with-open [transport# socket#]
-       (.open transport#)
-       ~@body)))
+         protocol# (TBinaryProtocol. socket#)]
+     (binding [*keyspace* ~keyspace
+               *client* (Cassandra$Client. protocol#)]
+       (with-open [transport# socket#]
+         (.open transport#)
+         ~@body))))
 
 ; TODO: move translation methods to another ns
 (defn bytes->str [bytes]
@@ -32,15 +33,48 @@
     (.setColumn (str->bytes column))))
 
 (defn get-generic [family k col]
-  (with-client [client [*host* *port*]]
-    (.get client *keyspace* k (make-column-path family col) clevel)))
+  (.get *client* *keyspace* k (make-column-path family col) clevel))
+
+(defn column->map [col]
+    {:name (bytes->str (.getName col)),
+     :value (bytes->str (.getValue col))
+     :timestamp (.getTimestamp col)})
 
 ; Usage:
-; (with-client [client ["localhost" 9160]]
+; (with-client [client ["localhost" 9160 "Keyspace1"]]
 ;   (get-column "Standard1" "ccc" "celery"))
 ;
 (defn get-column [family k col]
   (let [col (.getColumn (get-generic family k col))]
-    {:name (bytes->str (.getName col)),
-     :value (bytes->str (.getValue col))
-     :timestamp (.getTimestamp col)}))
+    (column->map col)))
+
+(defn insert [family k col value]
+  (.insert *client*
+    *keyspace*
+    k
+    (make-column-path family col)
+    (str->bytes value)
+    (System/currentTimeMillis)
+    clevel))
+
+(defn make-slice-range []
+  (doto (SliceRange.)
+    (.setStart (byte-array 0))
+    (.setFinish (byte-array 0))))
+
+(defn make-slice-predicate [slice-range]
+  (doto (SlicePredicate.)
+    (.setSlice_range slice-range)))
+
+; naive - ignores SuperColumn
+(defn get-row [column-family k]
+  (let [parent (ColumnParent. column-family)]
+    (map
+      #(column->map (.getColumn %))
+      (.get_slice *client*
+                  *keyspace*
+                  k
+                  parent
+                  (make-slice-predicate (make-slice-range))
+                  clevel))))
+
