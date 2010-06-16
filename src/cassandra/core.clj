3f7a81c898089111d44aea0cdee757e6f51284be
Colin Jones <trptcolin@gmail.com>
2010-06-16 17:03:40 -0500

    supercolumn support is messy, messy, messy... but it works

--- src/cassandra/core.clj
+++ src/cassandra/core.clj
@@ -4,7 +4,7 @@
     (org.apache.cassandra.thrift
       ConsistencyLevel Cassandra$Client ColumnPath
       SlicePredicate SliceRange ColumnParent
-      KeyRange)
+      KeyRange SuperColumn ColumnOrSuperColumn Column)
     (org.apache.thrift.transport TSocket)
     (org.apache.thrift.protocol TBinaryProtocol)))
 
@@ -34,6 +34,10 @@
    :value (bytes->str (.getValue col))
    :timestamp (.getTimestamp col)})
 
+(defn- supercolumn->map [col]
+  {:name (bytes->str (.getName col))
+   :value (map column->map (.getColumns col))})
+
 ; TODO: A bunch of dumb Java construction procedures here.
 ;       Should they be extracted?
 (defn- make-slice-range []
@@ -55,29 +59,59 @@
    (doto (make-column-path family)
      (.setColumn (str->bytes column)))))
 
+(defn- make-supercolumn-path [family column]
+    (doto (make-column-path family)
+      (.setSuper_column (str->bytes column))))
+
+(defn make-column
+  ([] (Column.))
+  ([col]
+   (make-column (col 0) (col 1)))
+  ([name value]
+   (Column. (str->bytes name) (str->bytes value) (get-current-microseconds))))
+
+(defn map->columns [column]
+  (map (fn [x] (make-column x)) column))
+
+(defn make-supercolumn
+  ([] (SuperColumn.))
+  ([name columns]
+   (SuperColumn. (str->bytes name) (map->columns columns))))
+
+(defn make-columnorsupercolumn
+  ([] (ColumnOrSuperColumn.))
+  ([col]
+    (if (map? (col 1))
+      (do
+        (doto (make-columnorsupercolumn)
+             (.setSuper_column (make-supercolumn (col 0) (col 1))))
+        )
+      (doto (make-columnorsupercolumn)
+        (.setColumn (make-column (col 0) (col 1)))))))
+
 (defn- get-range-slices [keyspace column-family]
-  (.get_range_slices
-    *client*
-    keyspace
-    (ColumnParent. column-family)
-    (make-slice-predicate)
-    (make-key-range "" "")
-    consistency-level))
+(.get_range_slices
+  *client*
+  keyspace
+  (ColumnParent. column-family)
+  (make-slice-predicate)
+  (make-key-range "" "")
+  consistency-level))
 
 (defn- get-records-in-column-family [keyspace cf]
-  (map
-    (fn [row] {:column-family cf :key (.getKey row) :columns (.getColumns row)})
-    (get-range-slices keyspace cf)))
+(map
+  (fn [row] {:column-family cf :key (.getKey row) :columns (.getColumns row)})
+  (get-range-slices keyspace cf)))
 
 (defn get-all-keyspaces
-  "Gets a sequence of the names of all keyspaces on *client*"
-  [] (seq (.describe_keyspaces *client*)))
+"Gets a sequence of the names of all keyspaces on *client*"
+[] (seq (.describe_keyspaces *client*)))
 
 ; TODO: Is this memoization a bad idea?
 ;       Currently, changing keyspaces requires a cassandra restart to re-read the xml
 ;       This strategy will obviously be problematic in other cases...
 (def memoized-get-all-keyspaces
-  (memoize get-all-keyspaces))
+(memoize get-all-keyspaces))
 
 
 ; TODO: The instance? check here seems wrong, but does seem to work...
@@ -85,40 +119,52 @@
 ;         but pass the instance? test?  That's what happens here.
 ;       Also, this doesn't check for java.util.Map as a key (admittedly rare)
 (defn- to-hash-map [h]
-  (apply hash-map
-    (interleave
-      (keys h)
-      (map
-        #(if (instance? java.util.Map %) (to-hash-map %) %)
-        (vals h)))))
+(apply hash-map
+  (interleave
+    (keys h)
+    (map
+      #(if (instance? java.util.Map %) (to-hash-map %) %)
+      (vals h)))))
 
 (defn describe-keyspace
-  "Given a keyspace name for *client*, gets a hash-map of keyspace names
-  mapped to a hash-map of keyspace settings"
-  [keyspace]
-  (to-hash-map
-    (if (some #{keyspace} (memoized-get-all-keyspaces))
-      (.describe_keyspace *client* keyspace)
-      nil)))
+"Given a keyspace name for *client*, gets a hash-map of keyspace names
+mapped to a hash-map of keyspace settings"
+[keyspace]
+(to-hash-map
+  (if (some #{keyspace} (memoized-get-all-keyspaces))
+    (.describe_keyspace *client* keyspace)
+    nil)))
+
+(defn vec->supercolumns [v]
+(make-columnorsupercolumn v))
 
 (defn insert
-  "Can insert a column, or an entire record as a set of columns"
-  ([family k value-map]
-    (doall
-      (map
-        #(insert family k (% 0) (% 1))
-        value-map)))
+"Can insert a column, or an entire record as a set of columns"
+([family k value-map]
+ (doall
+   (map
+     (fn [x]
+       (if (map? (x 1))
+         (do
+           (.batch_insert *client*
+             *keyspace*
+             k
+             {family [(vec->supercolumns x)]}
+             consistency-level)
+           )
+         (insert family k (x 0) (x 1))
+           ))
+       value-map)))
   ([family k col value] (insert *keyspace* family k col value))
   ([keyspace family k col value]
-   (.insert *client*
-     keyspace
-     k
-     (make-column-path family col)
-     (str->bytes value)
-     (get-current-microseconds)
-     consistency-level)))
+    (.insert *client*
+      keyspace
+      k
+      (make-column-path family col)
+      (str->bytes value)
+      (get-current-microseconds)
+      consistency-level)))
 
-; naive - ignores SuperColumn
 ; TODO: cache keyspaces to eliminate database calls + above
 (defn- column-family-exists?
   ([column-family] (column-family-exists? *keyspace* column-family))
@@ -126,13 +172,17 @@
    (some #{column-family}
      (keys (describe-keyspace keyspace)))))
 
+; naive - ignores SuperColumn
 (defn- get-columns
   ([column-family k] (get-columns *keyspace* column-family k))
   ([keyspace column-family k]
    (if (column-family-exists? keyspace column-family)
      (let [parent (ColumnParent. column-family)]
        (map
-         #(column->map (.getColumn %))
+         #(let [supercolumn (.getSuper_column %)]
+            (if supercolumn
+              (supercolumn->map supercolumn)
+              (column->map (.getColumn %))))
          (.get_slice *client*
            keyspace
            k
@@ -141,6 +191,14 @@
            consistency-level)))
      nil)))
 
+(defn mapcolumns->map [cols]
+  (reduce (fn [x y] (into x {(:name y) (:value y)})) {} cols))
+
+(defn name-value-reducer [x y]
+  (if (seq? (:value y))
+    (into x {(:name y) (mapcolumns->map (:value y))})
+    (into x {(:name y) (:value y)})))
+
 ; TODO: We discard timestamp information here
 ;       Is that something there's a genuine use case for?
 ;       Wanted to add it as metadata on the column value, but alas...
@@ -148,14 +206,8 @@
   "Get a record as a map of column names to values"
   ([column-family key] (get-record *keyspace* column-family key))
   ([keyspace column-family k]
-   (apply
-     hash-map
-     (reduce
-       (fn [x y] (conj x
-                   (:name y)
-                   (:value y)))
-       []
-       (get-columns keyspace column-family k)))))
+   (let [cols (get-columns keyspace column-family k)]
+     (reduce name-value-reducer {} cols))))
 
 (defn delete-record
   ([column-family k] (delete-record *keyspace* column-family k))
--- test/cassandra/core_test.clj
+++ test/cassandra/core_test.clj
@@ -3,27 +3,31 @@
         cassandra.test-helper
         clojure.test))
 
-
 (test-cassandra core-functionality
   ; TODO: Right now there's a manual step to create these keyspaces
   ;       It should be automatic (maybe a prompt if they already exist?)
   ;       This will probably mean running a script that populates the storage-conf.xml
   ;       Should it pull the test keyspaces back out at the end of the test run?
   ;       Should the tests fire up the Cassanda server automatically?
+
+
+
+  ; This should be happen before every test, but apparently use-fixtures
+  ;   doesn't do what we need. Roll our own?
   (clear-keyspace "CassandraClojureTestKeyspace1")
   (clear-keyspace "CassandraClojureTestKeyspace2")
 
-  (testing "get-all-keyspaces"
+  (testing "get all keyspaces"
     (let [keyspaces (get-all-keyspaces)]
       (is (= "CassandraClojureTestKeyspace1"
              (some #{"CassandraClojureTestKeyspace1"} keyspaces)))
       (is (= "CassandraClojureTestKeyspace2"
              (some #{"CassandraClojureTestKeyspace2"} keyspaces)))))
 
-  (testing "describing-nonexistent-keyspace"
+  (testing "describing nonexistent keyspace"
     (is (= {} (describe-keyspace "imaginary-keyspace"))))
 
-  (testing "describing-existing-keyspace"
+  (testing "describing existing keyspace"
     (let [keyspace (describe-keyspace "CassandraClojureTestKeyspace1")
           events (keyspace "Events")]
       (is (= "org.apache.cassandra.db.marshal.UTF8Type" (events "CompareWith")))
@@ -35,17 +39,17 @@
                       (events "Desc")))))
 
 
-  (testing "getting-empty-record-with-real-column-family"
+  (testing "getting empty record with real column family"
     (is (= {} (get-record "CassandraClojureTestKeyspace1" "Events" "bogus_key")))
     (in-keyspace "CassandraClojureTestKeyspace1"
       (is (= {} (get-record "Events" "bogus_key")))))
 
-  (testing "getting-empty-record-with-nonexistent-column-family"
+  (testing "getting empty record with nonexistent column family"
     (is (= {} (get-record "CassandraClojureTestKeyspace1" "fake_column_family" "bogus_key")))
     (in-keyspace "CassandraClojureTestKeyspace1"
       (is (= {} (get-record "fake_column_family" "bogus_key")))))
 
-  (testing "getting-existing-record"
+  (testing "getting existing record"
     (in-keyspace "CassandraClojureTestKeyspace1"
       (insert "People" "colin" "full_name" "Colin Jones")
       (insert "People" "colin" "location" "Chicagoland")
@@ -54,7 +58,7 @@
         (is (= "Colin Jones" (colin "full_name")))
         (is (= "Chicagoland" (colin "location"))))))
 
-  (testing "deleting-a-record"
+  (testing "deleting a record"
     (in-keyspace "CassandraClojureTestKeyspace1"
       (insert "People" "kathy" "full_name" "Kathy Jones")
       (is (= {"full_name" "Kathy Jones"} (get-record "People" "kathy")))
@@ -62,11 +66,36 @@
       (delete-record "People" "kathy")
       (is (= {} (get-record "People" "kathy")))))
 
-  (testing "inserting-a-record"
+  (testing "inserting a record"
     (in-keyspace "CassandraClojureTestKeyspace1"
       (insert "People" "oscar" {"full_name" "Oscar Jones", "location" "Chicagoland"})
       (let [oscar (get-record "People" "oscar")]
         (is (= {"full_name" "Oscar Jones",
                 "location", "Chicagoland"} (get-record "People" "oscar"))))))
+
+  (testing "with supercolumns"
+    (in-keyspace "CassandraClojureTestKeyspace2"
+
+      (testing "get existing keyspaces"
+        (let [keyspace (describe-keyspace "CassandraClojureTestKeyspace2")
+              events (keyspace "People")]
+          (is (re-matches #"(?s).*CassandraClojureTestKeyspace2\.People.*"
+                (events "Desc")))
+          (is (re-matches #"(?s).*Column Family Type: Super.*"
+                (events "Desc")))
+          (is (re-matches #"(?s).*Columns Sorted By: org.apache.cassandra.db.marshal.BytesType.*"
+                (events "Desc")))))
+
+      (testing "inserting and getting"
+        (let [molly { "address" {"city" "Mundelein"
+                                 "state" "IL"
+                                 "zip" "60060"}}]
+          (insert "People" "molly" molly)
+
+          (is (= molly (get-record "People" "molly")))))
+
+;      (testing "get nonexistent record (testing before)"
+;        (is (= {} (get-record "People" "molly"))))
+      ))
 
 )
