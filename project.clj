475330cb5d74102f6d79381c5964a6cff57bd3f3
Colin Jones <trptcolin@gmail.com>
2010-09-20 20:11:07 -0500

    That ns doesn't exist anymore. There's no main either.

--- project.clj
+++ project.clj
@@ -5,7 +5,5 @@
                  [org.slf4j/slf4j-api "1.5.8"]
                  [org.slf4j/slf4j-log4j12 "1.5.8"]
                  [org.clojars.trptcolin/apache-cassandra "0.6.2"]
-                 [org.clojars.trptcolin/libthrift "r917130"]]
-  :main clojandra.core)
-
+                 [org.clojars.trptcolin/libthrift "r917130"]])
 
