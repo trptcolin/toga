fc8b784ee19567d5a3f63b2d8c28da58e4154ee3
Colin Jones <trptcolin@gmail.com>
2010-09-28 18:34:20 -0500

    Update to Clojure 1.2.0 proper.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject toga "0.0.1"
   :description "Cassandra Client for Clojure"
-  :dependencies [[org.clojure/clojure "1.2.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
                  [log4j/log4j "1.2.14"]
                  [org.slf4j/slf4j-api "1.5.8"]
                  [org.slf4j/slf4j-log4j12 "1.5.8"]
