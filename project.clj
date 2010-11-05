ca3abf0bab332505d76377e22c16099557b9814d
Colin Jones <trptcolin@gmail.com>
2010-11-04 22:30:11 -0500

    Use Riptano repo for cassandra & thrift.

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
                  [log4j/log4j "1.2.14"]
                  [org.slf4j/slf4j-api "1.5.8"]
                  [org.slf4j/slf4j-log4j12 "1.5.8"]
-                 [org.clojars.trptcolin/apache-cassandra "0.6.2"]
-                 [org.clojars.trptcolin/libthrift "r917130"]]
+                 [org.apache.cassandra/apache-cassandra "0.6.5"]
+                 [libthrift "917130"]]
   :dev-dependencies [[com.stuartsierra/lazytest "1.0.0"]]
-  :repositories {"stuartsierra.com" "http://stuartsierra.com/maven2"})
+  :repositories {"stuartsierra.com" "http://stuartsierra.com/maven2"
+                 "riptano" "http://mvn.riptano.com/content/repositories/riptano"})
 
